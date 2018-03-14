fa3e0ae8d53206adaf3f97b1145e378c52bee57a
Jiulong Hu <me@hujiulong.com>
2018-03-14 19:00:05 +0800

    render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
-function h( tag, attrs, ...children ) {}
+function h( tag, attrs, ...children ) {
+    return {
+        tag,
+        attrs,
+        children
+    }
+}
 
-function foo() {}
+const node = <div className="a">a <span></span></div>;
 
-const node = <div className="a" onClick={foo}>a <span></span></div>
+function render( vnode, container ) {
+
+    if ( typeof vnode === 'string' ) {
+        let textNode = document.createTextNode( vnode );
+        return container.appendChild( textNode );
+    }
+
+    const dom = document.createElement( vnode.tag );
+
+    if ( vnode.attrs ) {
+        Object.keys( vnode.attrs ).forEach( key => {
+
+            // http://www.jackpu.com/you-qu-de-hua-ti-wei-shi-yao-jsxyong-classnameer-bu-shi-class/
+            // https://groups.google.com/forum/#!topic/reactjs/xovHWHGHPCA
+            if ( key === 'className' ) key = 'class';
+
+            dom.setAttribute( key, vnode.attrs[ key ] )
+        } );
+    }
+
+    vnode.children.forEach( child => render( child, dom ) );
+
+    return container.appendChild( dom );
+}
+
+render( node, document.getElementById( 'root' ) );
