0b0880cb188eec5c257603ccbd2ce2e77d326e7e
Jiulong Hu <me@hujiulong.com>
2018-03-26 00:34:51 +0800

    component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
-import React from './react.js'
-import ReactDOM from './react-dom.js'
+import React from './react'
+import ReactDOM from './react-dom'
 
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
-}
+// function Welcome(props) {
+//     return <h1>Hello, {props.name}</h1>;
+// }
 
-function App() {
-    return (
-        <div>
-            <Welcome name="Sara" />
-            <Welcome name="Cahal" />
-            <Welcome name="Edite" />
-        </div>
-    );
+// function App() {
+//     return (
+//         <div>
+//             <Welcome name="Sara" />
+//             <Welcome name="Cahal" />
+//             <Welcome name="Edite" />
+//         </div>
+//     );
+// }
+//
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById( 'root' )
+// );
+
+
+class Welcome extends React.Component {
+    constructor( props ) {
+        super( props );
+        this.state = {};
+    }
+    render() {
+        return <h1>Hello, {this.props.name}</h1>;
+    }
 }
 
 ReactDOM.render(
-    <App />,
+    <Welcome name="Jack" />,
     document.getElementById( 'root' )
 );
