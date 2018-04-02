d7b1677d72ab7c936896abcf70fd69731fd8a0ea
Jiulong Hu <me@hujiulong.com>
2018-04-02 22:40:29 +0800

    diff component

--- src/react/create-element.js
+++ src/react/create-element.js
@@ -2,18 +2,13 @@ import Component from './component.js'
 
 function createElement( tag, attrs, ...children ) {
 
-    // 类定义组件
-    if ( tag.prototype &&  tag.prototype.render ) {
-        return new tag( attrs );
-    // 函数定义组件
-    } else if ( typeof tag === 'function' ) {
-        return tag( attrs || {} );
-    }
+    attrs = attrs || {};
 
     return {
         tag,
         attrs,
-        children
+        children,
+        key: attrs.key || null
     }
 }
 
