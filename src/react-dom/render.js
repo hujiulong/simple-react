218fa694635bdd2c15de2ca1dd4903398662fd92
jiulong.hjl <jiulong.hjl@alibaba-inc.com>
2018-07-31 11:14:34 +0800

    clean up

--- src/react-dom/render.js
+++ src/react-dom/render.js
@@ -6,14 +6,14 @@ function createComponent( component, props ) {
     let inst;
 
     if ( component.prototype && component.prototype.render ) {
-		inst = new component( props );
-	} else {
-		inst = new Component( props );
-		inst.constructor = component;
-		inst.render = function() {
+            inst = new component( props );
+      } else {
+            inst = new Component( props );
+            inst.constructor = component;
+            inst.render = function() {
             return this.constructor( props );
         }
-	}
+      }
 
     return inst;
 }
@@ -27,10 +27,10 @@ function unmountComponent( component ) {
 function setComponentProps( component, props ) {
 
     if ( !component.base ) {
-		if ( component.componentWillMount ) component.componentWillMount();
-	} else if ( component.componentWillReceiveProps ) {
-		component.componentWillReceiveProps( props );
-	}
+            if ( component.componentWillMount ) component.componentWillMount();
+      } else if ( component.componentWillReceiveProps ) {
+            component.componentWillReceiveProps( props );
+      }
 
     component.props = props;
 
